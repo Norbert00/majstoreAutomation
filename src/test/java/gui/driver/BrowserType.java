5b1de4de0854c64268d79f29cb46876684519d7a
Norber00 <norbert.jurek@protonmail.com>
2020-06-04 11:55:00 +0200

    add browser type class holds browser type used in tests

--- src/test/java/gui/driver/BrowserType.java
+++ src/test/java/gui/driver/BrowserType.java
@@ -1,4 +1,16 @@
 package gui.driver;
 
 public enum BrowserType {
+
+    FIREFOX("firefox"),
+    CHROME("chrome");
+
+    private final String browser;
+
+    BrowserType(String browser) {
+        this.browser = browser;
+    }
+
+
+
 }
