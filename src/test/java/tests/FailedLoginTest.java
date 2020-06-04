ba56df2a5e117c262dc9fd4dadb05315bb68c5e7
Norber00 <norbert.jurek@protonmail.com>
2020-06-04 12:00:10 +0200

    add failed login test

--- src/test/java/tests/FailedLoginTest.java
+++ src/test/java/tests/FailedLoginTest.java
@@ -1,4 +1,25 @@
 package tests;
 
+import gui.driver.DriverUtils;
+import gui.pom.LoginPage;
+import org.testng.annotations.Test;
+
+import static gui.navigation.ApplicationURLs.LOGIN_URL;
+import static org.testng.Assert.assertTrue;
+
 public class FailedLoginTest {
+
+    @Test
+    public void asUserTryToLoginWithIncorrectLoginAndPassword() {
+
+        DriverUtils.setInitialConfiguration();
+        DriverUtils.navigateToPage(LOGIN_URL);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.userEnterIncorrectLoginAndPassword(LoginPage.getINCORRECTUSERNAME(),LoginPage.getINCORRECTPASSWORD());
+        assertTrue(loginPage.errorMessageIsDisplay());
+
+
+    }
+
 }
