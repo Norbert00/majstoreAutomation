6e6e49979542c446c1d1ca211d7bcf84b277d575
Norber00 <norbert.jurek@protonmail.com>
2020-06-04 11:58:57 +0200

    add base page to easyer initialize the driver and WebElements

--- src/test/java/gui/pom/BasePage.java
+++ src/test/java/gui/pom/BasePage.java
@@ -1,4 +1,13 @@
 package gui.pom;
 
+import gui.driver.DriverManager;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(DriverManager.getWebDriver(), this);
+    }
+
+
 }
