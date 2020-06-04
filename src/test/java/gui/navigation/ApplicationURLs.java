84ff5708b143815d432d2931e6f6e5b0b83babf4
Norber00 <norbert.jurek@protonmail.com>
2020-06-04 11:57:46 +0200

    add applicationURLs class to manage the relative urls

--- src/test/java/gui/navigation/ApplicationURLs.java
+++ src/test/java/gui/navigation/ApplicationURLs.java
@@ -3,5 +3,6 @@ package gui.navigation;
 public class ApplicationURLs {
 
     public static final String APPLICATION_URL = "http://localhost/majstore/";
+    public static final String LOGIN_URL = APPLICATION_URL + "/wp-login.php";
 
 }
