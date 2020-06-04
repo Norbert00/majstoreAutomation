df2ab1396b88887a4a5fd3327d5c18ef94abdd0c
Norber00 <norbert.jurek@protonmail.com>
2020-06-04 11:59:39 +0200

    add LoginPage class which holds all WebElements in login page and methods to mangae them

--- src/test/java/gui/pom/LoginPage.java
+++ src/test/java/gui/pom/LoginPage.java
@@ -1,4 +1,73 @@
 package gui.pom;
 
-public class LoginPage {
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+import static gui.wait.WaitForElement.waitUntilElementIsVisible;
+
+public class LoginPage extends BasePage {
+
+
+
+
+    @FindBy(id = "user_login")
+    private WebElement usernameOrEmailAddressInput;
+
+    @FindBy(id = "user_pass")
+    private WebElement passwordInput;
+
+    @FindBy(id = "wp-submit")
+    private WebElement LoginButton;
+
+    @FindBy(id = "login_error")
+    private WebElement errorMessageField;
+
+    private static final String INCORRECTUSERNAME = "adam";
+    private static final String INCORRECTPASSWORD = "321Start";
+    private static final String ERRORMESSAGE = "Error: Invalid Password.";
+
+
+    public static String getINCORRECTUSERNAME() {
+        return INCORRECTUSERNAME;
+    }
+    public static String getINCORRECTPASSWORD() {
+        return INCORRECTPASSWORD;
+    }
+
+
+    private void enterLoginToTheUsernameInput(String username) {
+        waitUntilElementIsVisible(usernameOrEmailAddressInput);
+        usernameOrEmailAddressInput.sendKeys(username);
+    }
+
+    private void enterPasswordInPasswordField(String password) {
+        waitUntilElementIsVisible(passwordInput);
+        passwordInput.sendKeys(password);
+    }
+
+    private void clickOnLoginButton() {
+        LoginButton.click();
+    }
+
+    private String getErrorMessageOut() {
+        waitUntilElementIsVisible(errorMessageField);
+        System.out.println(errorMessageField.getText());
+        return errorMessageField.getText();
+    }
+
+    public void userEnterIncorrectLoginAndPassword(String username, String password) {
+        enterLoginToTheUsernameInput(username);
+        enterPasswordInPasswordField(password);
+        clickOnLoginButton();
+    }
+
+    public boolean errorMessageIsDisplay() {
+        boolean errorMessageDisplay = (getErrorMessageOut().equals(ERRORMESSAGE)) ? true : false;
+        return errorMessageDisplay;
+    }
+
+
 }
